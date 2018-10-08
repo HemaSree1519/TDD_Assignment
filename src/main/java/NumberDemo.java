9b541c1b3bb544c64531fcd645be4d037f66b916
HemaSree1519 <khemasree96@gmail.com>
2018-10-08 08:42:37 -0700

    returns true for two

--- src/main/java/NumberDemo.java
+++ src/main/java/NumberDemo.java
@@ -1,6 +1,13 @@
 public class NumberDemo {
-
-    public boolean isPrime(int i) {
+private static final int ONE =1;
+private static final int TWO=2;
+    public boolean isPrime(int inputNumber) {
+        if (inputNumber == ONE) {
+            return false;
+        }
+        if(inputNumber == TWO) {
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 public class NumberTest {
     NumberDemo number= new NumberDemo();
@@ -9,5 +10,11 @@ public class NumberTest {
     public void returnFalseForOne()
     {
         assertFalse(number.isPrime(1));
+    }
+
+    @Test
+    public void returnTrueForTwo()
+    {
+        assertTrue(number.isPrime(2));
     }
 }
