07b223a955a4c212e235aca5629712f3ff7f1b09
HemaSree1519 <khemasree96@gmail.com>
2018-10-07 22:56:22 -0700

    returns 1 for 2nd element of series

--- src/test/java/FibonacciSeries.java
+++ src/test/java/FibonacciSeries.java
@@ -1,5 +1,17 @@
 public class FibonacciSeries {
+    private static final int ZERO=0;
+    private static final int ONE=1;
+    private static final int TWO=2;
+    private static final int THREE=3;
     public int nthNumber(int index) {
-        return 0;
+        if(index == ONE)
+        {
+            return ZERO;
+        }
+        else if (index == TWO)
+        {
+            return ONE;
+        }
+        return ZERO;
     }
 }
