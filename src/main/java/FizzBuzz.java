e28787d3ca3fb38a67b213cd10f1fa6bb5bebec8
HemaSree1519 <khemasree96@gmail.com>
2018-10-07 17:00:52 -0700

    returns Fizz for 3 multiple

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz {
 private static final int THREE=3;
 private static final int ZERO = 0;
     public String fizzOrBuzz(int inputNumber) {
-        if(inputNumber==3)
+        if(inputNumber % THREE == ZERO)
         {
             return "Fizz";
         }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,21 +4,24 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     FizzBuzz fizzBuzzObject = new FizzBuzz();
+
     @Test
-    public void returnsOneForOne()
-    {
-        assertEquals("1",fizzBuzzObject.fizzOrBuzz(1));
+    public void returnsOneForOne() {
+        assertEquals("1", fizzBuzzObject.fizzOrBuzz(1));
     }
 
     @Test
-    public void returnFizzFor3()
-    {
-        assertEquals("Fizz",fizzBuzzObject.fizzOrBuzz(3));
+    public void returnFizzFor3() {
+        assertEquals("Fizz", fizzBuzzObject.fizzOrBuzz(3));
     }
 
     @Test
-    public void returnBuzzFor5()
-    {
-        assertEquals("Buzz",fizzBuzzObject.fizzOrBuzz(5));
+    public void returnBuzzFor5() {
+        assertEquals("Buzz", fizzBuzzObject.fizzOrBuzz(5));
+    }
+
+    @Test
+    public void returnFizzFor3Multiples() {
+        assertEquals("Fizz", fizzBuzzObject.fizzOrBuzz(6));
     }
 }
