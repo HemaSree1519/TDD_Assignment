b4c7c6e126f3156ed650de4ec33a15b2683fd33c
HemaSree1519 <khemasree96@gmail.com>
2018-10-27 17:28:11 +0530

    Returns same value for dfferent objects

--- src/test/java/Assignment4/SingletonWithEnumTest.java
+++ src/test/java/Assignment4/SingletonWithEnumTest.java
@@ -5,11 +5,24 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SingletonWithEnumTest {
+    SingletonWithEnum Obj1 = SingletonWithEnum.INSTANCE;
+    ;
+    SingletonWithEnum Obj2 = SingletonWithEnum.INSTANCE;
+    ;
+
     @Test
-    public void shouldReturnOneForOne()
-    {
-        SingletonWithEnum Obj1 = SingletonWithEnum.INSTANCE;
+    public void shouldReturnOneForOne() {
+
         Obj1.setValue(1);
-        assertEquals(1,Obj1.getValue());
+        assertEquals(1, Obj1.getValue());
     }
+
+    @Test
+    public void shouldReturnSameValueForDifferentOjects() {
+        Obj2.setValue(2);
+        assertEquals(2, Obj1.getValue());
+    }
+
 }
+
+
