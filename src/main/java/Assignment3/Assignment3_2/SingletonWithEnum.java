b1da4b9d8201e4ae0f8f2e29cd7d5e2d8322df1e
HemaSree1519 <khemasree96@gmail.com>
2018-11-03 08:13:00 +0530

    Refactored the code by removing unneccessary tests

--- src/main/java/Assignment3/Assignment3_2/SingletonWithEnum.java
+++ src/main/java/Assignment3/Assignment3_2/SingletonWithEnum.java
@@ -1,15 +1,6 @@
 package Assignment3.Assignment3_2;
 
-public enum SingletonWithEnum {
-    INSTANCE;
-    int data;
-
-    public void setValue(int data) {
-        this.data = data;
-    }
-
-    public int getValue() {
-        return data;
-    }
+enum SingletonWithEnum {
+    INSTANCE
 }
 
--- src/test/java/Assignment3/Assignment3_2/SingletonWithEnumTest.java
+++ src/test/java/Assignment3/Assignment3_2/SingletonWithEnumTest.java
@@ -1,33 +1,16 @@
 package Assignment3.Assignment3_2;
 
-import Assignment3.Assignment3_2.SingletonWithEnum;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class SingletonWithEnumTest {
     SingletonWithEnum Obj1 = SingletonWithEnum.INSTANCE;
-    ;
     SingletonWithEnum Obj2 = SingletonWithEnum.INSTANCE;
-    ;
-
-    @Test
-    public void shouldReturnOneForOne() {
-
-        Obj1.setValue(1);
-        assertEquals(1, Obj1.getValue());
-    }
-
-    @Test
-    public void shouldReturnSameValueForDifferentOjects() {
-        Obj2.setValue(2);
-        assertEquals(2, Obj1.getValue());
-    }
 
     @Test
     public void shouldReturnTrueIfTwoObjectsReferSameInstance() {
-        assertTrue(Obj1.equals(Obj2));
+        assertEquals(Obj1, Obj2);
     }
 }
 
